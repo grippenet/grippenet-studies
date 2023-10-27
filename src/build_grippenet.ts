34c82c6941c5f04131554b8c8638ce382e46ba5c
Clément Turbelin <clement.turbelin@gmail.com>
2023-10-27 14:41:40 +0200

    grippenet builder

--- src/build_grippenet.ts
+++ src/build_grippenet.ts
@@ -1,10 +1,11 @@
 import { CommonStudyBuilder } from "../common/studies/common";
-import { buildMissing, study_exporter } from "../common/tools/exporter";
+import { buildMissing, json_export, study_exporter } from "../common/tools/exporter";
 
 import "../common/studies/common/languages/fr";
 import "./grippenet/languages";
 
 import { GrippenetStudyBuilder } from "./grippenet/study";
+import { extra_texts } from "./utils";
 
 const builder = new GrippenetStudyBuilder();
 
@@ -12,4 +13,10 @@ builder.build();
 
 const study = builder.getStudy();
 
-study_exporter([study]);
+study_exporter([study], {'check': true, 'missing': true, 'classNames': true});
+
+const extra = Object.fromEntries(Array.from(extra_texts.entries()).map((v) => {
+    return [v[0], Object.fromEntries(v[1].entries())];
+}));
+
+json_export('output/grippenet/extra_trans.json', extra);
