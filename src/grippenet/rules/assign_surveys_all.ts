458f58a11e82d37c4cac288fe8e01acd0458f6e1
Clément Turbelin <clement.turbelin@gmail.com>
2023-10-30 15:19:52 +0100

    add assign rules

---
+++ src/grippenet/rules/assign_surveys_all.ts
@@ -0,0 +1,27 @@
+import { StudyEngine as se } from "case-editor-tools/expression-utils/studyEngineExpressions";
+import { Expression } from "survey-engine/data_types";
+import { RuleSet } from "./base"
+import { intakeSurveyKey, vaccinationSurveyKey, weeklySurveyKey } from "../constants";
+
+const create = (): Expression[]=> {
+
+    const assignedSurveys = se.participantActions.assignedSurveys;
+    const out: Expression[] = [];
+    
+    const exp =  se.do(
+            assignedSurveys.remove(intakeSurveyKey, 'all'),
+            assignedSurveys.remove(weeklySurveyKey, 'all'),
+            assignedSurveys.remove(vaccinationSurveyKey, 'all'),
+            assignedSurveys.add(intakeSurveyKey, 'prio'),
+            assignedSurveys.add(vaccinationSurveyKey, 'prio'),
+        );
+
+    out.push(exp);
+   
+    return out;
+};
+
+export const ruleset: RuleSet = {
+    name : 'assign_survey_all',
+    rules: create
+}
---
+++ src/grippenet/rules/assign_surveys_tester.ts
@@ -0,0 +1,33 @@
+import { StudyEngine as se } from "case-editor-tools/expression-utils/studyEngineExpressions";
+import { Expression } from "survey-engine/data_types";
+import { RuleSet } from "./base"
+import { GrippenetFlags } from "../flags";
+import { intakeSurveyKey, vaccinationSurveyKey, weeklySurveyKey } from "../constants";
+
+const create = (): Expression[]=> {
+
+    const assignedSurveys = se.participantActions.assignedSurveys;
+    const hasParticipantFlagKeyAndValue = se.participantState.hasParticipantFlagKeyAndValue;
+    const testerFlag = GrippenetFlags.reminderTester;
+    const out: Expression[] = [];
+
+    
+    const exp = se.ifThen(
+        hasParticipantFlagKeyAndValue(testerFlag.key, testerFlag.values.yes),
+        se.do(
+            assignedSurveys.remove(intakeSurveyKey, 'all'),
+            assignedSurveys.remove(weeklySurveyKey, 'all'),
+            assignedSurveys.remove(vaccinationSurveyKey, 'all'),
+            assignedSurveys.add(intakeSurveyKey, 'prio'),
+            assignedSurveys.add(vaccinationSurveyKey, 'prio'),
+        ));
+
+    out.push(exp);
+   
+    return out;
+};
+
+export const ruleset: RuleSet = {
+    name : 'assign_survey_testers',
+    rules: create
+}
